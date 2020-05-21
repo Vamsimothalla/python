6c850989baad30d941efa9ad25e0e75bed4624dc
Vamsimothalla <42707756+Vamsimothalla@users.noreply.github.com>
2020-05-21 12:50:13 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
-import os
-import sys
+api_id = '1293341'
+api_hash = '951efa9687bd986c2947a4ac2a6b6b0c'
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+bot = TelegramClient('xxx', api_id, api_hash).start(bot_token='1184021844:AAG2Zhx-6GSRHKYgQWrbIWSAi2zOFN5Pdo8')
+@bot.on(events.NewMessage(pattern='/start'))
+async def send_welcome(event):
+   await event.reply('Howdy, how are you doing?')
 
-    from django.core.management import execute_from_command_line
+@bot.on(events.NewMessage)
+async def echo_all(event):
+   await event.reply(event.text)
 
-    execute_from_command_line(sys.argv)
+bot.run_until_disconnected()
