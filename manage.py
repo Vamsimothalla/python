dd3c559f634089f6430dbcb7e43211c0797b9259
Vamsimothalla <42707756+Vamsimothalla@users.noreply.github.com>
2020-05-21 12:51:26 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-api_id = '1293341'
-api_hash = '951efa9687bd986c2947a4ac2a6b6b0c'
+api_id = ''
+api_hash = ''
 
 bot = TelegramClient('xxx', api_id, api_hash).start(bot_token='1184021844:AAG2Zhx-6GSRHKYgQWrbIWSAi2zOFN5Pdo8')
 @bot.on(events.NewMessage(pattern='/start'))
